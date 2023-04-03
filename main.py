90af510388dac993babcc4e17c3bf5758a2b860a
Tal Carmel <talcarmel787@gmail.com>
2023-04-03 12:58:13 +1000

    cpu boats being placed is done

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import time
 import random
 
 board = []
+hiddenBoard = []
 x = 10
 y = 10
 cpuBoats = 0
@@ -13,9 +14,11 @@ boatLengths = {0:5, 1: 4, 2: 3, 3:2}
 
 def CreateBoard():
     global board
+    global hiddenBoard
     global x
     global y
     board = [['[ ]' for i in range(y)] for j in range(x)]
+    hiddenBoard= [['[ ]' for i in range(y)] for j in range(x)]
 
 def PrintBoard():
     j = 0
@@ -51,6 +54,43 @@ def PrintBoard():
         print(coordY[k] + newLine) # Print Y label
         k = k +1 
         newLine = ''
+
+
+def PrintCPUBoard():
+    j = 0
+    k = 0
+    count = 0
+    countX= 0
+    coordX = [['[ ]' for i in range(y)] for j in range(x)]
+    coordY = [['[ ]' for i in range(y)] for j in range(x)]
+    while j < x:
+        #Create X and Y labels array
+        coordX[j] = f'[{count}]'
+        while k < y:
+            coordY[k] = f'[{countX}]'
+            k = k + 1
+            countX = countX + 1
+        countX = 0
+        k = 0
+        count = count + 1
+        j = j + 1
+
+    # Print X label
+    newLine = ''
+    for item in coordX:
+        newLine = newLine + item
+    print("[+]"+newLine)
+    newLine = ''
+
+    # Print board
+    newLine = ''
+    for line in hiddenBoard:
+        for item in line:
+            newLine = newLine + item
+        print(coordY[k] + newLine) # Print Y label
+        k = k +1 
+        newLine = ''
+
 
 def NumberBoard():
     j = 0
@@ -91,9 +131,11 @@ def CPUBoats():
     print("CPUBoats")
 
 def CheckHit(x,y, cpuMove):
-    if (board[y][x] == '[=]'):
-        board[y][x] = '[X]'
+    if (board[y][x] == '[=]' and cpuMove):
+        board[y][x] = '[#]'
         return True
+    elif (hiddenBoard[y][x] == '[=]'):
+        board[y][x] = '[X]'
     elif (board[y][x] == '[ ]'):
         if(cpuMove):
             board[y][x] = '[-]'
@@ -105,6 +147,7 @@ def GameUI():
     print("\n=================================") 
     print(f"CPU Boats Left: {cpuBoats}")  
     print(f"CPU Bombed cell: {lastCpuMove}")    
+    # PrintCPUBoard()
     
 
 def Attack(x,y, cpuMove):
@@ -134,7 +177,19 @@ def CPU():
     
 
 def CreateCpuBoats():
-    print("createcpuboats")    
+    global hiddenBoard
+    placed = 0
+    for item, boat in boatLengths.items():   
+        boatX = random.randint(0,9-boat)
+        boatY = random.randint(0,9)
+        #plyInput = input("Enter the rotation boat coord: (in XY i.e. 43)")
+        i = 0
+        while i < boat: 
+            hiddenBoard[boatY][boatX + i] = "[=]"
+            i = i +1
+        os.system('cls')
+        PrintBoard()
+        placed = placed+1  
 
 def Move():
     os.system('cls')
@@ -159,6 +214,7 @@ CreateBoard()
 #    plyInput = input("Are you ready to start? y/n:\n")
 if(True):#"y" in plyInput):
     PlaceBoats()
+    CreateCpuBoats()
     alive = True
     plyInput = input("Enter first move (in XY i.e. 43)") 
     while (len(plyInput) != 2 or plyInput.isnumeric() is False):
